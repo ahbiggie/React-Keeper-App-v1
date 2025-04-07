d29fc861166cb2a7c002adc3361fca4b7cbd28c1
shaibu lawal yusuf <58580624+ahbiggie@users.noreply.github.com>
2025-04-07 10:52:18 +0100

    Add files via upload
    Initial commit for the keeper app UI using React components.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import Header from './Header';
+import Footer from "./Footer";
+import Notes from './Notes';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/components/Notes.js
@@ -0,0 +1,10 @@
+function Notes() {
+    return (
+        <div className="note">
+            <h1>Title</h1>
+            <p>There's some content here</p>
+        </div>
+    );
+}
+
+export default Notes;
